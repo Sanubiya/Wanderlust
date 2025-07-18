a8224f5031e0baf5f0b93a94732b74877cd86e40
Sanubiya Farid <sanubiyafarid@gmail.com>
2025-07-18 18:32:37 +0530

    Add new project files

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use((req,res,next)=>{
     next();
  })
  app.get("/",(req,res)=>{
-    res.render("/listings/index.js")
+    res.render("listings/welcome.ejs")
  })
 
 app.use("/listings",listingsRoute);
